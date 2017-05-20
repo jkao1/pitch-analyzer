6e0dfa88d3bf539add56a9d629748edbee4ab5ec
jkao1 <jkao1@stuy.edu>
2017-05-20 08:49:15 -0400

    csv writing works well

--- scripts/Driver.java
+++ scripts/Driver.java
@@ -3,69 +3,86 @@ import java.io.*;
 
 public class Driver {
 
+    private static double[][] toneStorer = new double[50][4];
+
     public static void main(String[] args)
     {
         File f0 = new File("/Users/jasonkao/Desktop/pitch-analyzer/f0");
         File[] files = f0.listFiles();
 
-        ArrayList<Double>[] scores = {
-            new ArrayList<Double>(),
-            new ArrayList<Double>(),
-            new ArrayList<Double>(),
-            new ArrayList<Double>(),
-        };
-
-        File[] zhu = {
+        File[] zhuFiles = {
             new File("/Users/jasonkao/Desktop/pitch-analyzer/zhu-tone1.txt"),
             new File("/Users/jasonkao/Desktop/pitch-analyzer/zhu-tone2.txt"),
             new File("/Users/jasonkao/Desktop/pitch-analyzer/zhu-tone3.txt"),
             new File("/Users/jasonkao/Desktop/pitch-analyzer/zhu-tone4.txt")
         };
 
-        try {
+        String dir = "/Users/jasonkao/Desktop/pitch-analyzer/f0/";
 
-            String dir = "/Users/jasonkao/Desktop/pitch-analyzer/f0/";
+        for ( File f : files ) {
 
-            for ( File f : files ) {
+            try {
 
-                Scanner name = new Scanner( f.getName() );
-                int userID = name.nextInt();
-                int tone = name.nextInt();
+                String[] fileParts = f.getName().split( "-" );
+                int userID = Integer.parseInt( fileParts[0] );
+                int tone = fileParts[1].charAt(0) - '0';
 
-                Scanner userFile = new Scanner( f );
-                Scanner zhuFile = new Scanner( zhu[tone - 1 ]);
+                Scanner userScan = new Scanner( f );
+                Scanner zhuScan = new Scanner( zhuFiles[ tone - 1 ]);
+                User user = new User( userScan, zhuScan );
 
-                User user = new User( userFile, zhuFile );
+                toneStorer[userID][tone - 1] = user.getRSquared();
 
-                scores[tone - 1].add( user.getRSquared() );
+            } catch (Exception e) {
+                System.out.println("!#EXCEPTION: Failed to read file " + f + ".");
             }
+        }
 
-        } catch(IOException e) {
-            System.out.println("IO Exception");
+        printOut( toneStorer );
+    }
+
+    public static void printOut(double[][] d)
+    {
+        for (double[] user : toneStorer) {
+            if ( takeSum(user) == 0.0 ) { // empty means 0.0
+                continue;
+            }
+            for (double tone : user) {
+                System.out.print(tone + ",");
+            }
+            System.out.println();
         }
     }
 
-public static void print2D(double[][] d)
-{
-    String output = "";
-    for (int i = 1; i < d.length; i++) {
-        output += "[";
-        for (double x : d[i]) {
+    private static double takeSum(double[] d) {
+        double output = 0.0;
+        for (double dub : d) {
+            output += dub;
+        }
+        return output;
+    }
+
+    public static void print2D(double[][] d)
+    {
+        String output = "";
+        for (int i = 1; i < d.length; i++) {
+            output += "[";
+            for (double x : d[i]) {
+                output += x + ", ";
+            }
+            output = output.substring(0, output.length() - 2) + "]\n";
+        }
+        System.out.println(output);
+    }
+
+    public static void print1D(Object[] o) {
+        String output = "";
+        for (Object x : o) {
             output += x + ", ";
         }
-        output = output.substring(0, output.length() - 2) + "]\n";
+        if (o.length > 0) {
+            output = output.substring(0, output.length() - 2);
+        }
+        System.out.println(output + "]");
     }
-    System.out.println(output);
-}
-
-public static void print1D(Object[] o) {
-    String output = "";
-    for (Object x : o) {
-        output += x + ", ";
-    }
-    if (o.length > 0) {
-        output = output.substring(0, output.length() - 2);
-    }
-    System.out.println(output + "]");
-}
 }
